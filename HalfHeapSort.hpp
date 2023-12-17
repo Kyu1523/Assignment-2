7832023508715470450a938e37f560aadcfbcbd6
Kenny Yu <k486498@gmail.com>
2023-12-16 23:22:55 -0500

    done

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -11,12 +11,12 @@ void percDown(std::vector<int>& heap, std::vector<int>::size_type hole){
     int child;
     int temp;
     int size = heap.size();
-    for(temp = std::move(heap[hole]); 2*hole+1 < size; hole = child){
-        child = 2 * hole + 1;
-        if(child != size-1 && heap[child] < heap[child+1]){
+    for(temp = std::move(heap[hole]); 2*hole < size; hole = child){
+        child = 2 * hole;
+        if(child != size-1 && heap[child] > heap[child+1]){
             ++child;
         }
-        if(temp < heap[child]){
+        if(temp > heap[child]){
             heap[hole] = std::move(heap[child]);
         }
         else{
@@ -26,13 +26,12 @@ void percDown(std::vector<int>& heap, std::vector<int>::size_type hole){
     heap[hole] = std::move(temp);
 }
 
-
 /**
  * @brief Builds the heap
  * @param:The heap
  */
 void buildHeap (std::vector<int>& heap){
-    for(int i = heap.size( )/2 - 1; i >= 0; --i ){ 
+    for(int i = heap.size( )/2 ; i > 0; --i ){ 
         percDown(heap,i);
     }
 }
@@ -42,10 +41,13 @@ int halfHeapSort(std::vector<int>& nums,int& duration){
     nums.push_back(std::move(nums[0]));
     buildHeap(nums);
     int size = (nums.size()-1)/2;
-    for(int i = nums.size()-1 ; i > size; --i){
-        std::swap(nums[0], nums[i]);
-        percDown(nums,0);
+    if(nums.size()%2 == 1){
+        size--;
+    }
+    for(int i = 1 ; i < size; ++i){
         nums.erase(nums.begin()+1);
+        std::swap(nums[0], nums[i]);
+        percDown(nums,1);
     }
     auto stop = high_resolution_clock::now();
     auto length = duration_cast<milliseconds>(stop-start);
