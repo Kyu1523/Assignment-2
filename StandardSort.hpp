5889d1a8940f1de60e21e456709ac204d4c0736a
Kenny Yu <k486498@gmail.com>
2023-12-16 19:43:20 -0500

    1

--- StandardSort.hpp
+++ StandardSort.hpp
@@ -7,10 +7,7 @@
 int standardSort ( std::vector<int>& nums, int& duration ){
     auto start = std::chrono::high_resolution_clock::now();
     std::vector<int>::iterator begin = nums.begin();
-    std::vector<int>::iterator end = nums.end() - nums.size()/2;
-    if(nums.size() != 0 && nums.size() % 2 != 0){       //if not even increment end by 1 to get median
-        end++;
-    }
+    std::vector<int>::iterator end = nums.end();
     std::sort(begin,end);
     auto stop = std::chrono::high_resolution_clock::now();
     auto length = std::chrono::duration_cast<milliseconds>(stop-start);
