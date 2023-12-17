a761f552364bc0b1998a5448507406343e0b607d
Kenny Yu <k486498@gmail.com>
2023-12-16 20:08:53 -0500

    a

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
-
+using namespace std::chrono;
 /**
  * @post:Recursise part of merge sort that splits the array into smaller arrays and then merges them to sort the data
  * @param nums: The vector that stores the integers to be sorted
@@ -26,13 +26,13 @@ void recursive_inplace_merge_sort(std::vector<int>::iterator begin,std::vector<i
  * @returns the median of the sorted vector
  */
 int inPlaceMergeSort (std::vector<int>& nums, int& duration ){
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
     std::vector<int>::iterator begin = nums.begin();
     std::vector<int>::iterator end = nums.end();
     recursive_inplace_merge_sort(begin,end);
-    auto stop = std::chrono::high_resolution_clock::now();
+    auto stop = high_resolution_clock::now();
     std::vector<int>::iterator result = begin + nums.size()/2;
-    auto length = std::chrono::duration_cast<milliseconds>(stop-start);
+    auto length = duration_cast<milliseconds>(stop-start);
     duration = length.count();
     return *result;
 }
